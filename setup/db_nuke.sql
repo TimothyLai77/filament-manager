d47f732610f288be2fe669c17abaf932f6e8407c
Timothy Lai <33475176+TimothyLai77@users.noreply.github.com>
2025-04-22 21:21:16 -0400

    nuke db script that works

--- setup/db_nuke.sql
+++ setup/db_nuke.sql
@@ -1,6 +1,15 @@
-/* 
-during the db creation, postgres likes to create names with all lower case
+/**
+Running: `psql postgres -f db_nuke.sql`
 */
-DROP USER filamentmanageruser;
-\c postgres;
-DROP DATABASE filamentmanagerdb WITH (FORCE);
+
+-- Killing all connections to the database
+SELECT pg_terminate_backend(pg_stat_activity.pid)
+FROM pg_stat_activity
+WHERE pg_stat_activity.datname = 'filamentmanagerdb'
+  AND pid <> pg_backend_pid();
+
+-- Dropping database
+DROP DATABASE IF EXISTS "filamentmanagerdb";
+
+-- Dropping role
+DROP ROLE IF EXISTS "filamentmanageruser";
