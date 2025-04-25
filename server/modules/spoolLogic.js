4b12a3190e94614fce3aa3a0ccfc4297a312e9aa
Timothy Lai <33475176+TimothyLai77@users.noreply.github.com>
2025-04-25 18:39:28 -0400

    testing

--- server/modules/spoolLogic.js
+++ server/modules/spoolLogic.js
@@ -2,8 +2,15 @@ import { Sequelize, col } from "sequelize";
 import uniqid from 'uniqid';
 import { Spool } from '../data/models/Spool.js'
 
-const getSpool = async () => {
-
+const getSpoolById = async (id) => {
+    try {
+        // only return the dataValues
+        const spool = await Spool.findByPk(id);
+        return spool.toJSON();
+    } catch (e) {
+        console.log(e)
+        return null;
+    }
 }
 
 
@@ -29,7 +36,12 @@ const createSpool = async (
         cost: cost,
         dateAdded: new Date()
     });
-    console.log(`Added: ${newSpool.id} to database.`)
+    console.log(`Added: ${newSpool.id} to database.`);
+
+    // toJSON() just returns the dataValues, and doesn't include the
+    // the extra data/ also means doesn't include the model object.
+    return newSpool.toJSON();
+
 }
 
 const deleteSpool = async () => {
@@ -45,4 +57,4 @@ const editSpool = async () => {
 }
 
 
-export { createSpool };
+export { createSpool, getSpoolById };
--- server/tests/logicTest.js
+++ server/tests/logicTest.js
@@ -2,22 +2,20 @@ import * as util from "../modules/spoolLogic.js";
 Object.assign(global, util);
 
 const runTests = async () => {
-    const list = await createSpools();
-    checkSpools(list);
+    await spoolCreationTest();
+
 }
 
-const checkSpools = async (list) => {
-    list.forEach(e => {
-        console.log(e.id)
-    });
-}
 
-const createSpools = async () => {
+
+const spoolCreationTest = async () => {
     try {
         const s1 = await createSpool('spool', 'creality', 'pla', 'black', 'matte', 1000.00, 22.12);
         const s2 = await createSpool('spool2', 'esun', 'asa', 'white', null, 1000.00, 25.34);
         const s3 = await createSpool('spool3', 'bambu', 'petg', 'purple', 'normal', 1000.00, 25.34);
-        return [s1, s2, s3];
+
+        const s1p = await getSpoolById(s1.id);
+        console.log(s1p.name == s1.name);
     } catch {
         return false;
     }
