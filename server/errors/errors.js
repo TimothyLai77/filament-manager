c7fa8316bba313a5bf352746cffdb68f4c8c107b
Timothy Lai <33475176+TimothyLai77@users.noreply.github.com>
2025-04-25 19:40:26 -0400

    typo

--- server/errors/errors.js
+++ server/errors/errors.js
@@ -1,3 +1,3 @@
 class NotEnoughFilamentError extends Error { };
 class SpoolNotFoundError extends Error { };
-export { NotEnoughFilamentErro, SpoolNotFoundError };
+export { NotEnoughFilamentError, SpoolNotFoundError };
