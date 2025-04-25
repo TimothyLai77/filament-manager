6d0698d5982d85504e86ca88d9b37e905a5179de
Timothy Lai <33475176+TimothyLai77@users.noreply.github.com>
2025-04-25 16:27:48 -0400

    more tests

--- server/tests/logicTest.js
+++ server/tests/logicTest.js
@@ -1,6 +1,8 @@
 import { createSpool } from "../modules/spoolLogic.js";
 const runTests = async () => {
     await createSpool('spool', 'creality', 'pla', 'black', 'matte', 1000.00, 22.12);
+    await createSpool('spool2', 'esun', 'asa', 'white', null, 1000.00, 25.34);
+    await createSpool('spool3', 'bambu', 'petg', 'purple', 'normal', 1000.00, 25.34);
 }
 
 export { runTests }
