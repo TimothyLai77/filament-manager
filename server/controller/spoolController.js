67978c3881f3494c8f8680f7913cb428cb67adb7
Timothy Lai <33475176+TimothyLai77@users.noreply.github.com>
2025-04-28 17:27:47 -0400

    getspoolbyid error handling

--- server/controller/spoolController.js
+++ server/controller/spoolController.js
@@ -8,10 +8,27 @@ const {
 } = require('../modules/spoolLogic.js')
 
 router.get('/spools', async (req, res) => {
-    let body = req.body;
-    console.log('GET receieved');
-    const spools = await getSpools();
-    res.send(spools);
+    try {
+        const spools = await getSpools();
+        res.send(spools), 200;
+    } catch (e) {
+        console.log(e)
+        res.status(500).send('Something went wrong.');
+    }
+
 })
+
+
+router.get('/spools/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const spool = await getSpoolById(id);
+        res.send(spool), 200
+    } catch (e) {
+        console.log(e)
+        res.status(500).send('Something went wrong.');
+    }
+})
+
 
 module.exports = router;
