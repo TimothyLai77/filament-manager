e7b75d46d4309c8714c5f82dabad1224f8d967ca
Timothy Lai <33475176+TimothyLai77@users.noreply.github.com>
2025-04-29 13:44:46 -0400

    proxy for backedn

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 8081,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080', // Backend server
+        changeOrigin: true, // Ensure the request appears to come from the frontend server
+      }
+    }
+  },
   plugins: [react()],
+  build: {
+    assetsDir: 'static',
+  },
 })
