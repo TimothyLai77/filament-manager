6528ad3ff171ec4c09ed6b490e31174eb38682b2
Timothy Lai <33475176+TimothyLai77@users.noreply.github.com>
2025-04-29 16:47:18 -0400

    styling table

--- frontend/src/components/SpoolList.jsx
+++ frontend/src/components/SpoolList.jsx
@@ -1,20 +1,21 @@
 import { loadableSpoolArrayAtom } from "../atoms.js"
-import { Table, Card,Stack, Button } from "@chakra-ui/react"
+import { Table, Card,Text, Button, HStack } from "@chakra-ui/react"
+import { MdModeEdit,MdLibraryAdd } from "react-icons/md";
 import { useAtom } from 'jotai'
 
 
 const SpoolList = () => {
   const [spools] = useAtom(loadableSpoolArrayAtom)
-  if (spools.state === 'hasError') console.error('something wrong with loading data')
+  if (spools.state === 'hasError') return <h1>Something really broke...</h1>
   if (spools.state === 'loading') return <h1>loading</h1>
   const spoolList = spools.data;
 
-  //console.log(spoolList[1]);
   return(
-  <Card.Root>
+  <Card.Root margin={5}>
     <Card.Body>
       <Card.Title>Current Filament</Card.Title>
-      <Table.Root>
+      <Table.ScrollArea borderWidth="0px">
+      <Table.Root interactive>
         <Table.Header>
           <Table.Row>
               <Table.ColumnHeader>Name</Table.ColumnHeader>
@@ -22,7 +23,8 @@ const SpoolList = () => {
               <Table.ColumnHeader>Material</Table.ColumnHeader>
               <Table.ColumnHeader>Colour</Table.ColumnHeader>
               <Table.ColumnHeader>Finish</Table.ColumnHeader>
-              <Table.ColumnHeader>Amount Left</Table.ColumnHeader>
+              <Table.ColumnHeader>Amount Left (g)</Table.ColumnHeader>
+              <Table.ColumnHeader align="end">Actions</Table.ColumnHeader> 
           </Table.Row>
         </Table.Header>
 
@@ -34,17 +36,24 @@ const SpoolList = () => {
               <Table.Cell>{spool.material}</Table.Cell>
               <Table.Cell>{spool.colour}</Table.Cell>
               <Table.Cell>{spool.finish}</Table.Cell>
-              <Table.Cell>{spool.filamentLeft}</Table.Cell>
-              <Table.Cell>
-                <Button>Edit</Button>
-              </Table.Cell>
-              <Table.Cell>
-                <Button>Create Job  </Button>
+              <Table.Cell>{spool.filamentLeft.toFixed(2)}</Table.Cell>
+              <Table.Cell align="end">
+                <HStack>
+                  <Button size="xs">
+                    <MdModeEdit/>
+                    Edit
+                  </Button>
+                  <Button size="xs">
+                    <MdLibraryAdd />
+                    Create Job 
+                  </Button>
+                </HStack>          
               </Table.Cell>
             </Table.Row>
         ))}
         </Table.Body>
       </Table.Root>
+      </Table.ScrollArea>
     </Card.Body>
   </Card.Root>
   )
