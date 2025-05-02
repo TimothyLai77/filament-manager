6c8b89cb193398e3dbc4281332deeaacd622428d
Timothy Lai <33475176+TimothyLai77@users.noreply.github.com>
2025-05-01 22:44:34 -0400

    home button component

--- frontend/src/pages/CreateSpoolPage.jsx
+++ frontend/src/pages/CreateSpoolPage.jsx
@@ -1,9 +1,11 @@
 import {NumberInput, Button, Input, Stack, Card, Text, Field, Fieldset} from '@chakra-ui/react'
 import SpoolCreationForm from '../components/SpoolCreationForm';
+import HomeButton from '../components/HomeButton.jsx';
 const CreateSpoolPage = () => {
 
   return (
     <>
+        <HomeButton />
         <SpoolCreationForm />
     </>
   )
