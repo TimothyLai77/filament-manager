cf633bda956dfc6cda497a9aad3627a79f6fc8c9
Timothy Lai <33475176+TimothyLai77@users.noreply.github.com>
2025-05-02 18:14:55 -0400

    cost per gram

--- frontend/src/components/SpoolDetailCard.jsx
+++ frontend/src/components/SpoolDetailCard.jsx
@@ -1,6 +1,6 @@
 import { selectedSpoolAtom, loadableSelectedSpoolDetailsAtom } from '../atoms.js';
 import { useAtom } from 'jotai';
-import {Text, Box, Card, Progress, Stat,FormatNumber, HStack, Stack } from '@chakra-ui/react';
+import { Text, Box, Card, Progress, Stat,FormatNumber, HStack, Stack } from '@chakra-ui/react';
 
 const SpoolDetailCard = () => {
     const [selectedSpool] = useAtom(selectedSpoolAtom);
@@ -19,34 +19,49 @@ const SpoolDetailCard = () => {
         const spoolData = spool.data
         //console.log(spoolData)
         const percentLeft = spoolData.filamentLeft/spoolData.initialWeight*100
+        const costPerGram = spoolData.cost / spoolData.initialWeight
         return (
             <>
                 <Card.Title textStyle="3xl">
                     {`Selected Spool: ${spoolData.name}`}
                 </Card.Title>
 
+                    <Stat.Root w={{ base: "100%", sm: "100%", md: "auto", lg: "50%" }} marginTop={3}>
+                    <Stat.Label textStyle="md">Filament Left / Total</Stat.Label>
+                    <Stat.ValueText textStyle="lg">
+                        <Box marginRight={1}>
+                            <FormatNumber value={spoolData.filamentLeft} style="unit" unit="gram" />
+                        </Box>
+                        {' / '}
+                        <Box marginLeft={1}>
 
-                <Stat.Root w={{ base: "100%", sm: "100%", md: "auto", lg: "50%" }} marginTop={3}>
-                <Stat.Label textStyle="md">Filament Left / Total</Stat.Label>
-                <Stat.ValueText textStyle="lg">
-                    <Box marginRight={1}>
-                        <FormatNumber value={spoolData.filamentLeft} style="unit" unit="gram" />
-                    </Box>
-                    {' / '}
-                    <Box marginLeft={1}>
+                            <FormatNumber value={`${spoolData.initialWeight}`} style="unit" unit="gram" />
+                        </Box>
+                    </Stat.ValueText>
+                            <Progress.Root value={percentLeft} shape="full">
+                            <HStack gap="5">
+                                <Progress.Track flex="1">
+                                    <Progress.Range />
+                                </Progress.Track>
+                                <Progress.ValueText>{percentLeft.toFixed(2)}%</Progress.ValueText>
+                            </HStack>
+                        </Progress.Root>
+                    </Stat.Root>
 
-                        <FormatNumber value={`${spoolData.initialWeight}`} style="unit" unit="gram" />
-                    </Box>
-                </Stat.ValueText>
-                        <Progress.Root value={percentLeft} shape="full">
-                        <HStack gap="5">
-                            <Progress.Track flex="1">
-                                <Progress.Range />
-                            </Progress.Track>
-                            <Progress.ValueText>{percentLeft.toFixed(2)}%</Progress.ValueText>
-                        </HStack>
-                    </Progress.Root>
-                </Stat.Root>
+       
+                    <Stat.Root maxWidth="25%" marginTop={3}>
+                    <Stat.Label textStyle="md">{`Cost ($) / g`}</Stat.Label>
+                    <Stat.ValueText textStyle="lg">
+                        <Box marginRight={1}>
+                            <FormatNumber value={costPerGram} style="currency" currency="USD" />
+                        </Box>
+                    </Stat.ValueText>
+         
+                    </Stat.Root>
+              
+
+
+                
                 <Stack marginTop={3}>
                     <Box>
                         <Text as="span" textStyle="lg" fontWeight="bold">Material: </Text>
