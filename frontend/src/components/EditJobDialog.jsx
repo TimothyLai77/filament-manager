b71b37ba9129818f78c4b8c4ad515a330aada93f
Timothy Lai <33475176+TimothyLai77@users.noreply.github.com>
2025-05-14 17:54:27 -0400

    frontend side logic

--- frontend/src/components/EditJobDialog.jsx
+++ frontend/src/components/EditJobDialog.jsx
@@ -1,26 +1,56 @@
-import { Input, Fieldset, Field, Box, CloseButton, Dialog, Portal, Button } from '@chakra-ui/react'
+import { Tag, Text, Input, Fieldset, Field, Box, CloseButton, Dialog, Portal, Button } from '@chakra-ui/react'
 import { MdModeEdit } from "react-icons/md";
 import { useAtom } from 'jotai';
-import { asyncEditJobAtom, loadableSelectedSpoolDetailsAtom } from '../atoms'
-import { useState } from 'react';
+import { asyncEditJobAtom, finalSelectedSpoolAtom, loadableSelectedSpoolDetailsAtom } from '../atoms'
+import { useEffect, useState } from 'react';
 const EditJobDialog = ({ job }) => {
     const [, setPayload] = useAtom(asyncEditJobAtom);
 
-
+    const [selectedSpool] = useAtom(finalSelectedSpoolAtom);
     const [name, setName] = useState(job.name);
     const [filamentUsed, setFilamentUsed] = useState(job.filamentAmountUsed);
     const [cost, setCost] = useState(job.cost);
 
+    const initialValues = {
+        name: job.name,
+        filamentAmountUsed: job.filamentAmountUsed,
+        cost: job.cost
+    };
 
-    // pain
+    let costPerGram = 0.0;
+    if (selectedSpool.state === 'hasData') {
+        costPerGram = selectedSpool.data.cost / selectedSpool.data.initialWeight;
+    }
+
+    const resetFields = () => {
+        setName(initialValues.name);
+        setFilamentUsed(initialValues.filamentAmountUsed)
+        setCost(initialValues.cost);
+    }
+
+    // update the price when amount is changed
+    useEffect(() => {
+        setCost(costPerGram * filamentUsed);
+    }, [filamentUsed])
+
+
     const verifyPayload = (payload) => {
-
+        if (payload.jobId == '' || !payload.jobId) return false;
+        if (payload.name == '') return false;
+        if (payload.filamentAmountUsed < 0.0 || isNaN(payload.filamentAmountUsed)) return false;
+        if (payload.cost < 0.0 || isNaN(payload.cost)) return false;
+        return true;
     }
 
     const handleSave = () => {
-        const payload = {}
+        const payload = {
+            jobId: job.id,
+            name: name,
+            filamentAmountUsed: parseFloat(filamentUsed),
+            cost: parseFloat(cost)
+        }
         if (verifyPayload(payload)) {
-            //console.log('valid payload')
+            console.log('valid payload')
             setPayload(payload)
         } else {
             console.log('invalid payload', payload);
@@ -29,7 +59,7 @@ const EditJobDialog = ({ job }) => {
 
     const form = () => {
         return (
-            <Fieldset.Root>
+            <Fieldset.Root marginTop={5}>
                 <Field.Root>
                     <Field.Label>Name</Field.Label>
                     <Input
@@ -43,7 +73,7 @@ const EditJobDialog = ({ job }) => {
                         value={filamentUsed}
                     />
 
-                    <Field.Label>Cost</Field.Label>
+                    <Field.Label>Cost (override if needed)</Field.Label>
                     <Input
                         onChange={(e) => setCost(e.target.value)}
                         value={cost}
@@ -57,7 +87,7 @@ const EditJobDialog = ({ job }) => {
     }
 
     return (
-        <Dialog.Root size="xl" initialFocusEl="null">
+        <Dialog.Root size="xl" initialFocusEl="null" onExitComplete={resetFields}>
             <Dialog.Trigger asChild>
                 <Box>
                     <Button size="xs"><MdModeEdit />Edit Job</Button>
@@ -69,8 +99,15 @@ const EditJobDialog = ({ job }) => {
                     <Dialog.Content>
                         <Dialog.Header>
                             <Dialog.Title>Edit Job: {`${job.name}`}</Dialog.Title>
+                            <Tag.Root>
+                                <Tag.Label>
+                                    {job.id}
+                                </Tag.Label>
+                            </Tag.Root>
                         </Dialog.Header>
                         <Dialog.Body>
+
+                            <Text>Cost/Gram : ${costPerGram}</Text>
                             {form()}
                         </Dialog.Body>
                         <Dialog.Footer>
