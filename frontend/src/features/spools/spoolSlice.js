96bb4de40e05f95a259a19d4951010960c246b92
timothy <lai.timothy77@gmail.com>
2025-11-17 18:20:09 -0500

    asyncthunk and reducers for fetch finished spools

--- frontend/src/features/spools/spoolSlice.js
+++ frontend/src/features/spools/spoolSlice.js
@@ -3,13 +3,28 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 const initialState = {
     spoolList: [],
     spoolDetails: {},
-    selectedSpool: null,
+    selectedSpool: null, // todo: decide if i actually need/want this.
+    finishedSpoolList: [],
     loading: false,
     error: null
 }
 
 
+// Get the list of finished/empty spools
+export const fetchFinishedSpoolsList = createAsyncThunk(
+    'api/finished-spools',
+    async () => {
+        try {
+            const response = await fetch('api/finished-spools');
+            return response.json();
+        } catch (err) {
+            return err;
+        }
+    }
+)
 
+
+// Get the details of a spool given the spool's ID
 export const fetchSpoolById = createAsyncThunk(
     'api/spools/:id',
     async (spoolId) => {
@@ -47,6 +62,7 @@ export const spoolSlice = createSlice({
     },
     extraReducers: (builder) => {
         builder
+            // FETCH ACTIVE SPOOLS
             .addCase(fetchActiveSpoolList.pending, (state) => {
                 state.loading = true;
             })
@@ -61,7 +77,7 @@ export const spoolSlice = createSlice({
                 state.error = action.payload
             })
 
-
+            // FETCH SPOOL DETAILS BY ID
             .addCase(fetchSpoolById.pending, (state) => {
                 state.loading = true;
             })
@@ -70,6 +86,19 @@ export const spoolSlice = createSlice({
                 state.spoolDetails = action.payload;
             })
             .addCase(fetchSpoolById.rejected, (state, action) => {
+                state.error = action.payload
+            })
+
+
+            // FETCH FINISHED SPOOLS LIST
+            .addCase(fetchFinishedSpoolsList.pending, (state) => {
+                state.loading = true;
+            })
+            .addCase(fetchFinishedSpoolsList.fulfilled, (state, action) => {
+                state.loading = false;
+                state.finishedSpoolList = action.payload;
+            })
+            .addCase(fetchFinishedSpoolsList.rejected, (state, action) => {
                 state.error = action.payload
             })
     }
