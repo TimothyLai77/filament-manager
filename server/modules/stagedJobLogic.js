d15748c00801c854a91070849c43f3a5523a407d
timothy <lai.timothy77@gmail.com>
2025-12-08 15:10:44 -0500

    added route and logic for deleting staged jobs

--- server/modules/stagedJobLogic.js
+++ server/modules/stagedJobLogic.js
@@ -2,15 +2,24 @@ import { DataTypes } from 'sequelize';
 import db from '../models/index.js'
 // dereference the stagedjob model object
 const { StagedJob } = db;
-
 import { generateJobId } from './jobLogic.js';
+
+/**
+ * Get all staged jobs from the database, and put them in a list
+ * @returns the list of all staged jobs in the database
+ */
 const getStagedJobs = async () => {
     return await StagedJob.findAll();
 }
 
+/**
+ * add staged job to the database
+ * @param {json payload} payload 
+ * @returns returns the new stagejob object back on success.
+ */
 const addStagedJob = async (payload) => {
     try {
-        console.log(payload)
+        //console.log(payload)
         const newJob = await StagedJob.create({
             id: generateJobId(),
             name: payload.name,
@@ -26,8 +35,23 @@ const addStagedJob = async (payload) => {
 
 }
 
+/**
+ * destroy staged job given the job's id
+ * @param {id of staged job to delete} id 
+ * @returns nothing, but returns an error on error. kinda bad design, oh well.
+ */
+const deleteStagedJob = async (id) => {
+    try {
+        const stagedJob = await StagedJob.findByPk(id)
+        await stagedJob.destroy();
+    } catch (error) {
+        return error;
+    }
+}
+
 
 export {
     getStagedJobs,
-    addStagedJob
+    addStagedJob,
+    deleteStagedJob
 }
--- server/routes/stagedJobs.js
+++ server/routes/stagedJobs.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getStagedJobs, addStagedJob } = require('../modules/stagedJobLogic')
+const { getStagedJobs, addStagedJob, deleteStagedJob } = require('../modules/stagedJobLogic')
 router.get('/', async (req, res) => {
     try {
         console.log('GET: /stagedJobs');
@@ -16,6 +16,19 @@ router.post('/', async (req, res) => {
         console.log('POST: /stagedjobs');
         const newJob = await addStagedJob(req.body)
         res.status(200).send(newJob)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+/**
+ * route to delete the staged job given the id. 
+ */
+router.delete('/:id', async (req, res) => {
+    try {
+        console.log(`DELETE: /stagedjobs/${req.params.jobId}`);
+        await deleteStagedJob(req.params.jobId);
+        res.status(200).end();
     } catch (error) {
         res.status(500).send(error)
     }
