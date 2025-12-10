97faf9e44a3bef68a581e6c8e308e74c3521481b
timothy <lai.timothy77@gmail.com>
2025-12-10 17:42:50 -0500

    basic card design, still need to fix zooming in and out

--- frontend/src/components/stagedJobComponents/StagedJobList.jsx
+++ frontend/src/components/stagedJobComponents/StagedJobList.jsx
@@ -1,18 +1,25 @@
+import { VStack } from "@chakra-ui/react"
 import { fetchStagedJobs } from "@/features/stagedJobs/fetchStagedJobsSlice"
 import { useSelector } from "react-redux"
+import StagedJobListEntry from "./StagedJobListEntry"
 const StagedJobList = () => {
     const { stagedJobList, loading, error } = useSelector((state) => state.fetchStagedJobs)
     if (loading) return <h1>loading...</h1>
     if (error) return <h1>error</h1>
     console.log(stagedJobList)
     return (
-        <ul>
-            {
-                stagedJobList.map(job => {
-                    return <li key={job.id}> {`${job.id} | ${job.name} | ${job.filamentUsed} | ${job.date} `} </li>
-                })
-            }
-        </ul>
+        <>
+            <VStack>
+                {
+                    stagedJobList.map(job => {
+                        return <StagedJobListEntry key={job.id} id={job.id} name={job.name} filamentUsed={job.filamentUsed} date={job.date} />
+                    })
+
+                }
+            </VStack>
+
+        </>
+
 
     );
 }
---
+++ frontend/src/components/stagedJobComponents/StagedJobListEntry.jsx
@@ -0,0 +1,41 @@
+import { Button, Card, Heading, Tag, Text, HStack, Spacer, VStack } from "@chakra-ui/react"
+import { MdOutlineDelete, MdCheck } from "react-icons/md";
+import { useSelector } from "react-redux"
+const StagedJobListEntry = ({ id, name, filamentUsed, date }) => {
+
+    return (
+        <Card.Root size="sm" minW={{ base: "100%", sm: "400px", md: "85%" }} flexDirection="row">
+
+            <Card.Body>
+                <HStack flexDirection={"row"} gap={10} flexWrap={"wrap"} >
+                    <VStack align="flex-start" spacing={0} minW="100px">
+                        <Text fontWeight={"bold"} size="xl">{name}</Text>
+                        <Tag.Root>
+                            <Tag.Label>{id}</Tag.Label>
+                        </Tag.Root>
+                    </VStack>
+
+                    <Text>Filament Used: {filamentUsed}g</Text>
+                    <Text>{date}</Text>
+                    <Spacer />
+                    <HStack>
+                        <Button size={'sm'}>
+                            <MdCheck />
+                            Edit & Commit
+                        </Button>
+                        <Button size={'sm'} colorPalette={"red"}>
+                            <MdOutlineDelete />
+                            Delete
+                        </Button>
+                    </HStack>
+
+                </HStack>
+
+
+            </Card.Body>
+        </Card.Root>
+
+    )
+}
+
+export default StagedJobListEntry
