7d1e4d2f5511a9027d0e986bafee4a0a455ea55c
timothy <lai.timothy77@gmail.com>
2025-12-15 19:10:05 -0500

    staged jobs sorted by date

--- frontend/src/components/stagedJobComponents/StagedJobList.jsx
+++ frontend/src/components/stagedJobComponents/StagedJobList.jsx
@@ -1,12 +1,14 @@
 import { VStack } from "@chakra-ui/react"
 import { fetchStagedJobs } from "@/features/stagedJobs/fetchStagedJobsSlice"
 import { useSelector } from "react-redux"
+import dayjs from "dayjs"
 import StagedJobListEntry from "./StagedJobListEntry"
+
 const StagedJobList = () => {
     const { stagedJobList, loading, error } = useSelector((state) => state.fetchStagedJobs)
     if (loading) return <h1>loading...</h1>
     if (error) return <h1>error</h1>
-    const formattedList = stagedJobList.map(job => ({
+    let formattedList = stagedJobList.map(job => ({
         // copy everything over from the current job
         ...job,
         // change the datestring into a JS date object
@@ -14,19 +16,38 @@ const StagedJobList = () => {
     }));
 
     formattedList.sort((a, b) => {
-        return a.date - b.date
+        return b.date - a.date
     });
     //console.log(formattedList)
+    formattedList = formattedList.map(job => ({
+        ...job,
+        formattedDate: dayjs(job.date).format('ddd MMM D')
+    }))
+    const jobsByDate = formattedList.reduce((acc, job) => {
+        if (!acc[job.formattedDate]) {
+            acc[job.formattedDate] = []
+        }
+        acc[job.formattedDate].push(job)
+        return acc
+    }, {})
+
     return (
         <>
-            <VStack>
-                {
-                    formattedList.map(job => {
-                        return <StagedJobListEntry key={job.id} id={job.id} name={job.name} filamentUsed={job.filamentUsed} date={job.date} />
-                    })
+            {
+                Object.keys(jobsByDate).map(day => {
+                    return (
+                        <>
+                            <h1>{day}</h1>
+                            {
+                                jobsByDate[day].map(job => {
+                                    return <StagedJobListEntry key={job.id} id={job.id} name={job.name} filamentUsed={job.filamentUsed} date={job.date} />
+                                })
+                            }
+                        </>
+                    )
+                })
+            }
 
-                }
-            </VStack>
 
         </>
 
