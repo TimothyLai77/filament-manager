59f26a1adbdf5cd15433711095544dc946733e3e
timothy <lai.timothy77@gmail.com>
2025-12-17 19:14:53 -0500

    VERY BUGGY: but basic implementation of getting job details on page

--- frontend/src/pages/CommitStagedJobPage.jsx
+++ frontend/src/pages/CommitStagedJobPage.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+import TopNavBar from "@/components/TopNavBar";
+import { Box, Heading, Text } from "@chakra-ui/react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
-
-
+import { useDispatch, useSelector } from "react-redux";
+import { fetchStagedJob } from "@/features/stagedJobs/fetchStagedJobsSlice";
 
 const CommitStagedJobPage = () => {
+    //TODO: technically the job object should be passed in through react-router-dom. I could use that object passed, but either way, if the state is lost from refreshing
+    // i would still need to use the redux async thunk to re-fetch the data anyways. so idk...
     const { jobId } = useParams();
-    //todo: probably some check to see if jobid is a staged job or not.
-    // just check if it has a spoolId property or somethign
-    return <h1>commit page for {jobId}</h1>
+    const dispatch = useDispatch();
+    const { loading, error, stagedJobDetail } = useSelector((state) => state.fetchStagedJobs)
+    // todo: really buggy. if you send a staged job to the server. and then try to edit & commit, the stagedjobdetail doesn't load fully. if you refresh it'll refetch and the state
+    // getss set properly. pls fix.
+    useEffect(() => {
+        dispatch(fetchStagedJob(jobId));
+    }, [dispatch])
+
+    if (loading || loading == null) return <h1>loading...</h1>
+    if (error) return <h1>error</h1>
+    console.log(stagedJobDetail)
+    return (
+        <>
+            <TopNavBar />
+            <Box>
+                <Heading>Edit and Commit {jobId}</Heading>
+                <Text>Name: {stagedJobDetail.name}</Text>
+                <Text>Filament Used: {stagedJobDetail.filamentUsed}g</Text>
+                <Text>{stagedJobDetail.date}</Text>
+            </Box>
+
+        </>
+    )
 }
 
 export default CommitStagedJobPage;
