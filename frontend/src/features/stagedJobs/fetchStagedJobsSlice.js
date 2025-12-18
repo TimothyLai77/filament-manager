1ea46382548addc82c24633ee9010e4ba5e80c91
timothy <lai.timothy77@gmail.com>
2025-12-17 19:03:57 -0500

    async thunk for getting staged job by id

--- frontend/src/features/stagedJobs/fetchStagedJobsSlice.js
+++ frontend/src/features/stagedJobs/fetchStagedJobsSlice.js
@@ -4,8 +4,23 @@ import { act } from "react";
 const initialState = {
     loading: null,
     error: null,
-    stagedJobList: []
+    stagedJobList: [],
+    stagedJobDetail: null,
 }
+
+
+export const fetchStagedJob = createAsyncThunk(
+    '/api/stagedjobs/:id',
+    async (id) => {
+        try {
+            const response = await fetch(`/api/stagedJobs/${id}`)
+            return response.json();
+        } catch (error) {
+            return error
+        }
+    }
+)
+
 
 export const fetchStagedJobs = createAsyncThunk(
     '/api//stagedJobs',
@@ -27,6 +42,7 @@ export const fetchStagedJobsSlice = createSlice({
     },
     extraReducers: (builder) => {
         builder
+            // GETTING ALL JOBS FROM DB
             .addCase(fetchStagedJobs.pending, (state) => {
                 state.loading = true;
                 state.error = null;
@@ -37,6 +53,22 @@ export const fetchStagedJobsSlice = createSlice({
                 state.stagedJobList = action.payload
             })
             .addCase(fetchStagedJobs.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
+            })
+
+            // GETTING ONE JOB BY ID
+            .addCase(fetchStagedJob.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+                state.stagedJobDetail = null;
+            })
+            .addCase(fetchStagedJob.fulfilled, (state, action) => {
+                state.loading = false;
+                state.error = false;
+                state.stagedJobDetail = action.payload
+            })
+            .addCase(fetchStagedJob.rejected, (state, action) => {
                 state.loading = false;
                 state.error = action.payload;
             })
