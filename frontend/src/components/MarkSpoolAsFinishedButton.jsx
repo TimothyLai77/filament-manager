57b45c5e0c197d7b785812e8e141787c2dcadf95
timothy <lai.timothy77@gmail.com>
2025-12-22 19:20:59 -0500

    Fixed bug with marking spool as finished, and viewing details on another spool. Had an old useeffect i forgot to remove

--- frontend/src/components/MarkSpoolAsFinishedButton.jsx
+++ frontend/src/components/MarkSpoolAsFinishedButton.jsx
@@ -18,9 +18,10 @@ const MarkSpoolAsFinishedButton = () => {
     const { successMarkedFinished, loading, error } = useSelector((state) => state.markSpoolFinished)
     const navigate = useNavigate()
     const handleConfirm = async () => {
+        console.log('handle confirm run')
         try {
             // dispatch the call. unwrap the result. if there is no error navigatge back to the root page
-            const result = await dispatch(markSpoolAsFinished(spoolId)).unwrap();
+            await dispatch(markSpoolAsFinished(spoolId)).unwrap();
             navigate('/')
         } catch (rejectedValueOrSerializedError) {
             // idk what to do...
@@ -29,14 +30,6 @@ const MarkSpoolAsFinishedButton = () => {
 
     }
 
-    useEffect(() => {
-        if (successMarkedFinished == null) return;
-        if (successMarkedFinished) {
-            navigate('/'); // spool marked successfully go back to main page
-        } else {
-            console.log('spool did not mark as finished successfully')
-        }
-    }, [successMarkedFinished])
 
     if (loading) return <></>
     if (error) return <h1>error</h1>
